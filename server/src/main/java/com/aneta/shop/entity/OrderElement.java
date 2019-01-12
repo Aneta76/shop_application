6a4aaa2a2d9fefe1a7dda76c8a137d34e227f604
Aneta76 <aneta_d88@o2.pl>
2019-01-12 14:24:20 +0100

    added OrderElement entity

---
+++ server/src/main/java/com/aneta/shop/entity/OrderElement.java
@@ -0,0 +1,42 @@
+package com.aneta.shop.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class OrderElement extends AbstractEntity {
+
+    @ManyToOne
+    private Order order;
+
+    @OneToMany
+    @JoinColumn(name = "id_product")
+    private Product product;
+
+    @Column
+    private Long quantity;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}

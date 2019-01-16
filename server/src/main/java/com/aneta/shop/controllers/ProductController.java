db406480a6178d4d69c545c394c5aded250c3eb7
Aneta76 <aneta_d88@o2.pl>
2019-01-16 20:37:54 +0100

    added ProductController

--- server/src/main/java/com/aneta/shop/controllers/ProductController.java
+++ server/src/main/java/com/aneta/shop/controllers/ProductController.java
@@ -2,8 +2,6 @@ package com.aneta.shop.controllers;
 
 import com.aneta.shop.entity.Product;
 import com.aneta.shop.service.ProductService;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +16,10 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping(value = "/product/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public Product getProduct(@PathVariable Long id){
+    @RequestMapping(path = "/product/{id}")
+    public Product getProduct(@PathVariable Long id) {
         Product oneProduct = productService.getOneProduct(id);
         return oneProduct;
     }
 }
+

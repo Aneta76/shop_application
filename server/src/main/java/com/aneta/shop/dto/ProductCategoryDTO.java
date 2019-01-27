f37bfd26d45269e920f52561bbbf6d850b590693
Aneta76 <aneta_d88@o2.pl>
2019-01-27 14:38:04 +0100

    added DTO files

---
+++ server/src/main/java/com/aneta/shop/dto/ProductCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.aneta.shop.dto;
+
+public class ProductCategoryDTO extends AbstractDTO {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ server/src/main/java/com/aneta/shop/dto/ProductDTO.java
@@ -0,0 +1,47 @@
+package com.aneta.shop.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDTO extends AbstractDTO {
+
+    private String name;
+
+    private String description;
+
+    private ProductCategoryDTO category;
+
+    private BigDecimal price;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductCategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategoryDTO category) {
+        this.category = category;
+    }
+
+}

9b0f2e1c0fe7a60667a48b1df17c584e4a8838e5
Aneta76 <aneta_d88@o2.pl>
2019-01-27 15:21:14 +0100

    added ProductService

--- server/src/main/java/com/aneta/shop/service/ProductService.java
+++ server/src/main/java/com/aneta/shop/service/ProductService.java
@@ -1,19 +1,10 @@
 package com.aneta.shop.service;
 
+import com.aneta.shop.dto.ProductDTO;
 import com.aneta.shop.entity.Product;
 
-import java.util.Collection;
-
-public interface ProductService {
-
-    Collection displayAll();
-
-    Product addProduct(Product product);
-
-    Product getOneProduct(Long id);
-
-    Product updateProduct(Product product);
-
-    void deleteProduct(Long id);
-
+public interface ProductService extends AbstractService<Product, ProductDTO> {
 }
+
+
+

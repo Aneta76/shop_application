09ce1f4d47bf7876b4ba0cbb5a133df7c82e464c
Aneta76 <aneta_d88@o2.pl>
2019-01-27 15:20:03 +0100

    added AbstractService

---
+++ server/src/main/java/com/aneta/shop/service/AbstractService.java
@@ -0,0 +1,18 @@
+package com.aneta.shop.service;
+
+import com.aneta.shop.dto.AbstractDTO;
+import com.aneta.shop.entity.AbstractEntity;
+
+import java.util.Collection;
+
+public interface AbstractService<ENTITY extends AbstractEntity, DTO extends AbstractDTO> {
+    DTO update(DTO dto);
+
+    DTO findById(Long id);
+
+    DTO save(DTO entity);
+
+    void delete(Long id);
+
+    Collection<DTO> getAll();
+}

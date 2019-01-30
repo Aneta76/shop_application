eb8284174d69a4f0e8954d9f28b0011e3db4e153
Aneta76 <aneta_d88@o2.pl>
2019-01-30 19:41:00 +0100

    added ProductCategoryService interface

---
+++ server/src/main/java/com/aneta/shop/service/ProductCategoryService.java
@@ -0,0 +1,7 @@
+package com.aneta.shop.service;
+
+import com.aneta.shop.dto.ProductCategoryDTO;
+import com.aneta.shop.entity.ProductCategory;
+
+public interface ProductCategoryService extends AbstractService<ProductCategory, ProductCategoryDTO> {
+}

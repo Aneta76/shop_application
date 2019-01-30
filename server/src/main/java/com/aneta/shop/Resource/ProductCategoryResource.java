5ba6f27632d1bb67901d68bc5b2a6820510770b4
Aneta76 <aneta_d88@o2.pl>
2019-01-30 19:59:28 +0100

    added ProductCategoryResource

---
+++ server/src/main/java/com/aneta/shop/Resource/ProductCategoryResource.java
@@ -0,0 +1,23 @@
+package com.aneta.shop.Resource;
+
+import com.aneta.shop.dto.ProductCategoryDTO;
+import com.aneta.shop.entity.ProductCategory;
+import com.aneta.shop.service.ProductCategoryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/category")
+public class ProductCategoryResource extends AbstractResource<ProductCategory, ProductCategoryDTO> {
+
+    private final ProductCategoryService productCategoryService;
+
+    public ProductCategoryResource(ProductCategoryService productCategoryService) {
+        this.productCategoryService = productCategoryService;
+    }
+
+    @Override
+    public ProductCategoryService getService() {
+        return productCategoryService;
+    }
+}

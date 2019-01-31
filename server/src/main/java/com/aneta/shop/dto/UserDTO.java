44900155b23e3d18825d29cd634acbc6e3be64ef
Aneta76 <aneta_d88@o2.pl>
2019-01-31 20:05:33 +0100

    added UserDTO

---
+++ server/src/main/java/com/aneta/shop/dto/UserDTO.java
@@ -0,0 +1,46 @@
+package com.aneta.shop.dto;
+
+import com.aneta.shop.entity.Role;
+
+public class UserDTO extends AbstractDTO {
+
+    private String lastName;
+
+    private String firstName;
+
+    private String email;
+
+    private Role role;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}

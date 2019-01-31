60d80dd71e7d4bf2ad1af1b9693c5c2117799e21
Aneta76 <aneta_d88@o2.pl>
2019-01-31 19:30:32 +0100

    added WarehouseItemService interface

---
+++ server/src/main/java/com/aneta/shop/service/WarehouseItemService.java
@@ -0,0 +1,7 @@
+package com.aneta.shop.service;
+
+import com.aneta.shop.dto.WarehouseItemDTO;
+import com.aneta.shop.entity.WarehouseItem;
+
+public interface WarehouseItemService extends AbstractService<WarehouseItem, WarehouseItemDTO> {
+}

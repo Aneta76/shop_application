18bc140dda7383ec9ec077082748a12354511259
Aneta76 <aneta_d88@o2.pl>
2019-01-31 20:07:51 +0100

    added UserRepository

---
+++ server/src/main/java/com/aneta/shop/respository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aneta.shop.respository;
+
+import com.aneta.shop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

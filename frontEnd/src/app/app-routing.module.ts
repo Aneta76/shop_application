f2c15b185166269711ee94f6a6bc2076a1630747
Aneta76 <aneta_d88@o2.pl>
2019-02-07 20:59:54 +0100

    angular fight tbc..

---
+++ frontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+
+
+export class AppRouting {
+}
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './admin-panel/products/products.component';
+import { ProductCategoriesComponent } from './admin-panel/product-categories/product-categories.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { MainComponent } from './layout/main/main.component';
+import {AppRouting} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductCategoriesComponent,
+    AdminPanelComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]

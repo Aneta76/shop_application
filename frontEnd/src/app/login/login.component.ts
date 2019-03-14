cb3894bdf589d8e76ff23245022422ff7a82f75e
Aneta76 <aneta_d88@o2.pl>
2019-03-14 21:31:40 +0100

    added login template

---
+++ frontEnd/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./../app.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string = null;
+  password: string = null;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  logIn() {
+
+  }
+}

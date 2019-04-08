47449ebade75aaa0fc115814c5a35972b2b5e8ec
Aneta76 <aneta_d88@o2.pl>
2019-04-08 21:56:14 +0200

    added productCategories component

--- frontEnd/src/app/admin-panel/product-categories/product-categories.component.ts
+++ frontEnd/src/app/admin-panel/product-categories/product-categories.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductCategoryModel} from '../../shared/model/product-category.model';
+import {ProductCategoryService} from '../../shared/service/productCategory.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-categories',
   templateUrl: './product-categories.component.html',
-  styleUrls: ['./product-categories.component.css']
+  styleUrls: ['./../../app.component.css']
 })
 export class ProductCategoriesComponent implements OnInit {
 
-  constructor() { }
+  productCategories: Array<ProductCategoryModel> = [];
+
+  constructor(private productCategoryService: ProductCategoryService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    /* tslint:disable:no-string-literal */
+    this.productCategories = this.route.snapshot.data['productCategories'];
+    /* tslint:enable:no-string-literal */
   }
 
 }

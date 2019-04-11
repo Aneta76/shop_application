6d419dce897d0ec0ac9705113cc6aa261c330487
Aneta76 <aneta_d88@o2.pl>
2019-04-11 21:21:02 +0200

    added productsByCategory component

---
+++ frontEnd/src/app/admin-panel/products-by-category/products-by-category.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductModel} from '../../shared/model/product.model';
+import {ActivatedRoute} from '@angular/router';
+import {ProductService} from '../../shared/service/product.service';
+
+@Component({
+  selector: 'app-products-by-category',
+  templateUrl: './products-by-category.component.html',
+  styleUrls: ['./../products/products.component.css']
+})
+export class ProductsByCategoryComponent implements OnInit {
+  private productsByCategory: Array<ProductModel> = [];
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.productsByCategory = this.route.snapshot.data['productsByCategory'];
+  }
+
+}

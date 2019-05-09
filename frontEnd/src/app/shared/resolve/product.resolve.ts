ebe95a6d53f56cad295d2a6b28bc8dd4df8f6ff0
Aneta76 <aneta_d88@o2.pl>
2019-05-09 21:30:21 +0200

    minor changes e.g removed unused imports

--- frontEnd/src/app/shared/resolve/product.resolve.ts
+++ frontEnd/src/app/shared/resolve/product.resolve.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRoute, ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
 import {ProductService} from '../service/product.service';
 
 @Injectable()
